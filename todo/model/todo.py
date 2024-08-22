7d1b7df03cb1bd03477bec12bd6abe2a5d8450c8
juan sebastian lopez yarce <jlopezyarce@gmail.com>
2024-08-22 12:53:29 -0500

    I add the special method __str__ that return the code_id and the title

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -8,11 +8,11 @@ class Todo:
         self.tags = []
     
     def mark_completed(self):
-
         self.completed = True
 
     def add_tag(self, tag: str):
-        
         if tag not in self.tags:
-
             self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
